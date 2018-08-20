ca791a5d66054156b762bda317a6797a47886996
uniqueimaginate <uniqueimaginate@gmail.com>
2018-08-20 05:09:39 +0000

    error_correction

--- db/migrate/20180815063609_change_column_name.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnName < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :notes, :user, :user_email_no
-    rename_column :comments, :user, :user_email_co
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180813135653) do
-
+ActiveRecord::Schema.define(version: 20180815073217) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "content"
@@ -20,7 +19,6 @@ ActiveRecord::Schema.define(version: 20180813135653) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
     t.index ["note_id"], name: "index_comments_on_note_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
